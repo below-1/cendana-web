e77e1120c58096554008d8d3a6f4ad8e60fcaa2e
jordan meta <jordanmeta@mail.com>
2021-08-11 12:02:39 +0800

    Use FilterEntity in product-list.page

--- src/pages/product/product-list.page.vue
+++ src/pages/product/product-list.page.vue
@@ -5,79 +5,68 @@
       <q-toolbar-title class="text-weight-bold">
         Data {{ ENTITY_NAME }}
       </q-toolbar-title>
+      <q-input v-model="params.keyword" hide-hint placeholder="keyword..." />
       <q-btn flat color="primary" label="tambah" icon="add" :to="CREATE_URL" />
     </q-toolbar>
 
     <loading-pane v-if="showLoading" />
 
-    <section v-else class="q-px-lg">
-      <q-table
-        flat
-        hide-pagination
-        :columns="COLUMNS"
-        :rows="listResult.items"
-        :rows-per-page-options="[0]"
-      >
-        <template v-slot:body-cell-actions="props">
-          <q-td :props="props">
-            <q-btn
-              icon="delete"
-              color="red"
-              flat size="xs"
-              @click="onRemove(props.row)"
-            />
-            <q-btn
-              :to="generateUpdateURL(props.row.id)"
-              icon="edit" color="primary" flat size="xs" />
-          </q-td>
-        </template>
-      </q-table>
-    </section>
+    <template v-else>
+      <section class="q-px-lg">
+        <q-table
+          flat
+          hide-pagination
+          :columns="COLUMNS"
+          :rows="products.items"
+          :rows-per-page-options="[0]"
+        >
+          <template v-slot:body-cell-actions="props">
+            <q-td :props="props">
+              <q-btn
+                icon="delete"
+                color="red"
+                flat size="xs"
+                @click="onRemove(props.row)"
+              />
+              <q-btn
+                :to="generateUpdateURL(props.row.id)"
+                icon="edit" color="primary" flat size="xs" />
+            </q-td>
+          </template>
+        </q-table>
+      </section>
 
-    <section class="q-px-lg">
-      <q-toolbar class="text-grey-8 q-py-lg">
-        <q-select
-          :options="[10, 25, 50]"
-          v-model.number="params.per_page"
-          label="jumlah item per halaman"
-          dense
-          outlined
-          class="q-mr-lg"
-          style="min-width: 200px;"/>
-        <q-input
-          :model-value="params.page + 1"
-          @update:model-value="v => {
-            params.page = v - 1;
-          }"
-          :shadow-text="`/${listResult.totalPage}`"
-          :suffix="`/${listResult.totalPage}`"
-          type="number"
-          :max="listResult.totalPage - 1"
-          :min="1"
-          label="halaman"
-          dense
-          outlined
-          style="max-width: 100px;"/>
-      </q-toolbar>
-    </section>
+      <section class="q-px-lg">
+        <q-toolbar class="text-grey-8 q-py-lg">
+          <pagination
+            v-model:page="params.page"
+            v-model:per-page="params.perPage"
+            :total-page="products.totalPage"
+          />
+        </q-toolbar>
+      </section>
+    </template>
+
 
   </q-page>
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from 'vue';
-import { useListEntity, useRemoveEntity } from 'src/compose/entity';
+import { defineComponent, computed, onMounted } from 'vue';
+import { useFilterEntity, useRemoveEntity } from 'src/compose/entity';
 import LoadingPane from 'components/loading-pane.vue';
+import Pagination from 'components/pagination.vue'
 import {
   ENTITY_NAME,
   BASE_APP_URL,
   BASE_API_URL,
   COLUMNS
-} from 'src/data/product';
+} from 'src/data/product'
 
 export default defineComponent({
   components: {
     LoadingPane,
+    Pagination
   },
   setup() {
     const CREATE_URL = BASE_APP_URL + '/create';
@@ -88,12 +77,17 @@ export default defineComponent({
 
     const {
       params,
-      result: listResult,
-      getEntityList,
-    } = useListEntity({ 
-      name: ENTITY_NAME,
-      url: BASE_API_URL 
-    });
+      getEntities: getProducts,
+      result: products
+    } = useFilterEntity({
+      url: BASE_API_URL,
+      name: 'Produk',
+      initialParams: {
+        keyword: ''
+      }
+    })
+
+    onMounted(getProducts)
 
     const {
       result: removeResult,
@@ -102,17 +96,17 @@ export default defineComponent({
 
     const onRemove = (item: any) => {
       promptRemove(`${BASE_API_URL}/${item.id}`, item.title)
-        .then(() => getEntityList());
+        .then(() => getProducts())
     }
 
-    const showLoading = computed(() => listResult.value.type == 'loading' || removeResult.value.type == 'loading');
+    const showLoading = computed(() => products.value.type == 'loading' || removeResult.value.type == 'loading');
 
     return {
       COLUMNS,
       ENTITY_NAME,
       CREATE_URL,
       params,
-      listResult,
+      products,
       generateUpdateURL,
       onRemove,
       showLoading,
